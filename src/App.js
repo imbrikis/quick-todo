44e6adae9b8ecfea837188bd755b458d2cd6b2cf
imbrikis <imbrikis@yahoo.com>
2020-12-07 22:45:49 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const endpoint = 'https://jsonbox.io/box_' + process.env.REACT_APP_JSONBOX_DB_ID
+
+const App = () => {
+
+  const [items, setItems] = useState([])
+  const [value, setValue] = useState('')
+
+  const fetchData = async () => {
+    const { data, status } = await axios.get(endpoint + '/items') 
+    if (status === 200) {
+      setItems(data)
+    }
+  }
+
+  const deleteItem = async id => {
+    await axios.delete(endpoint + `/items/${id}`) 
+    fetchData()
+  }
+
+  const addItem = async e => {
+    e.preventDefault()
+    if (e === '') {
+      return 
+    }
+
+    await axios.post(endpoint + "/items", { text: value }) 
+    setValue('')
+    fetchData()
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <form onSubmit={addItem} className="add-item-container">
+        <input className="input-field" type="text" value={value} onChange={e => setValue(e.target.value)} placeholder="Enter task item..." />
+        <button className="btn" type="submit">add</button>
+      </form>
+      {
+        items.map((item, index) => {
+          return (
+            <div className="items-container" key="item._id">
+              <div className="list-item-text">{`${index + 1}. ${item.text}`}</div>
+              <button className="delete-item-icon" onClick={() => deleteItem(item._id)}>
+                <i className="trash alternate outline icon list-item-icon"></i>
+              </button>
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
