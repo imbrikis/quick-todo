9664629e83d08265733cc3ad0d2022c7c0b010e4
imbrikis <imbrikis@yahoo.com>
2020-12-13 12:10:31 -0700

    add styling to edit controls / add media queries

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
 
   const [items, setItems] = useState([])
   const [value, setValue] = useState('')
+  const [isVisible, setIsVisible] = useState(false)
 
   const fetchData = async () => {
     const { data, status } = await axios.get(endpoint + '/items') 
@@ -15,20 +16,26 @@ const App = () => {
     }
   }
 
+  const addItem = async e => {
+    e.preventDefault()
+    if (e === '') {
+      return 
+    } else {
+      await axios.post(endpoint + "/items", { text: value }) 
+      setValue('')
+      fetchData()
+    }
+  }
+
   const deleteItem = async id => {
     await axios.delete(endpoint + `/items/${id}`) 
     fetchData()
   }
 
-  const addItem = async e => {
-    e.preventDefault()
-    if (e === '') {
-      return 
-    }
-
-    await axios.post(endpoint + "/items", { text: value }) 
-    setValue('')
-    fetchData()
+  const showEditControls = () => {
+    return (
+      <div className="edit-controls-container">EDIT CONTROLS CONTAINER</div>
+    )
   }
 
   useEffect(() => {
@@ -36,19 +43,20 @@ const App = () => {
   }, [])
 
   return (
-    <div>
+    <div className="app-container">
       <h1>To-Do List</h1>
       <form onSubmit={addItem} className="add-item-container">
         <input className="input-field" type="text" value={value} onChange={e => setValue(e.target.value)} placeholder="Enter task item..." />
         <button className="btn" type="submit">add</button>
       </form>
+      <div className={`edit-controls-container ${!isVisible ? "not-visible" : "visible"}`}>EDIT CONTROLS CONTAINER</div>
       {
         items.map((item, index) => {
           return (
             <div className="items-container" key="item._id">
               <div className="list-item-text">{`${index + 1}. ${item.text}`}</div>
-              <button className="delete-item-icon" onClick={() => deleteItem(item._id)}>
-                <i className="trash alternate outline icon list-item-icon"></i>
+              <button className="delete-item-icon" onClick={() => setIsVisible(!isVisible)}> {/*onClick={() => deleteItem(item._id)}*/}
+                <i className="pencil alternate icon list-item-icon"></i>
               </button>
             </div>
           )
